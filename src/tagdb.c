c9c49f464261e2df7cde058a5855a5da8c6606a5
Omar-Hasbini <umar.hasbini@gmail.om>
2025-06-03 10:32:03 +0200

    Add home check

--- src/tagdb.c
+++ src/tagdb.c
@@ -1,43 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <json-c/json.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <json-c/json.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pwd.h>
+#include "tagdb.h"
 
-int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        fprintf(stderr, "Error: you didn't provide any arguments or commands\n");
-        return 1;
-    } else if (argc == 2) {
-        if (strcmp(argv[1], "list") == 0) {
+int add_tag(const char *filename, const char *tag) {
 
-        } else {
-            fprintf(stderr, "Error: unknown command\n");
-            return 1;
-        }
-    } else if (argc == 3) {
-        if (strcmp(argv[1], "list") == 0) {
-        
-        } else if (strcmp(argv[1], "search") == 0) {
+    json_object* db = load_db();
+}
 
-        }
-        else {
-            fprintf(stderr, "Error: unknown command\n");
-            return 1;
-        }
-    } else if (argc == 4) {
-        if (strcmp(argv[1], "add") == 0) {
+int remove_tag(const char *filename, const char *tag) {
+    json_object* db = load_db();
+}
 
-        }
-        else if (strcmp(argv[1], "remove") == 0) {
+int search_by_tag(const char *tag) {
+    json_object* db = load_db();
+}
 
-        } else {
-            fprintf(stderr, "Error: unknown command\n");
-            return 1;
-        }
+int list_all_tags() {
+    json_object* db = load_db();
+}
+
+int list_file_tags(const char *filename) {
+    json_object* db = load_db();
+}
+
+struct json_object* load_db() {
+    
+}
+
+int save_db() {
+
+}
+
+int check_tagdb_exists() {
+    char dir[] = "~/home/.filetags/tags.json";
+    FILE fp* = fopen(dir);
+    if (fp) {
+        return 0;
     } else {
-        fprintf(stderr, "Error: you provided too many arguments\n");
         return 1;
     }
+}
 
-    return 0;
+int check_file_exists() {
+    // Source: https://stackoverflow.com/questions/308695/how-do-i-concatenate-const-literal-strings-in-c
+
+    char* home[] = get_home;
+
+    if (!home) {
+        return 1;  
+    }
+
+    FILE fp* = fopen(tags_file_dir);
+    if (fp) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+// Source: https://community.unix.com/t/getting-home-directory/248085/2
+
+char* get_home() {
+    struct passwd *pwd; 
+    if ( (pwd=getpwuid(getuid())) != NULL )
+    {
+        returns strdup(pwd->pw_dir);
+    } else {
+        char* home_env = getenv("HOME");
+        if (home_env != NULL) {
+            return strdup(home_env);
+        }
+    }
+
+    return NULL;
 }
