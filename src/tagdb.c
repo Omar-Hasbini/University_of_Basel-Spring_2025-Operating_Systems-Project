0634af66769124bdc48447fef492e7aabc49f78d
Omar-Hasbini <umar.hasbini@gmail.om>
2025-06-05 09:43:06 +0200

    finish list all tags

--- src/tagdb.c
+++ src/tagdb.c
@@ -151,7 +151,7 @@ int save_db(json_object* db) {
     const char* json_as_string = json_object_to_json_string_ext(db, JSON_C_TO_STRING_PRETTY);
 
     // Add \n so that the JSON file ends with a new line (for compatiblity purposes)
-    fprintf(fp, "%s\n",json_as_string);
+    fprintf(fp, "%s\n", json_as_string);
 
     fclose(fp);
     free(full_path);
@@ -159,8 +159,9 @@ int save_db(json_object* db) {
 }
 
 int add_tag(const char *filename, const char *tag) {
-    // Entry in the JSON DB to keep track of all existing tags
-    const char* all_tags = "__meta_all_tags__";
+    // Entry in the JSON DB to keep track of all existing tags,
+    //  "/" is usually reserved Linux Filesystems and so can never collide
+    const char* all_tags = "/__all_tags__";
 
     if (!check_file_exists(filename)) {
         fprintf(stderr, "Error: file does not exist.\n");
@@ -171,12 +172,6 @@ int add_tag(const char *filename, const char *tag) {
         fprintf(stderr, "Error: Tag is empty or is too long (max 255 char).\n");
         return -1;
     }
-
-    // Unlikely this will happen with most file systems as "__" is reserved
-    if (strcmp(filename, "__meta_all_tags__") == 0) {
-        fprintf(stderr, "Error: reserved filename.\n");
-        return -1;
-    }   
 
     json_object* db = load_tag_db();
     if (!db) {
@@ -192,17 +187,17 @@ int add_tag(const char *filename, const char *tag) {
         json_object_array_add(tags_array, json_object_new_string(tag));
         json_object_object_add(db, filename, tags_array);
     } else {
-        int tag_exists_globally = 0;     
+        int tag_exists_already = 0;     
         for (int i = 0; i < json_object_array_length(file_entry); i++ ) {
             json_object* current_tag = json_object_array_get_idx(file_entry, i);
 
             if (strcmp(json_object_get_string(current_tag), tag) == 0) {
-                tag_exists_globally = 1;
+                tag_exists_already = 1;
                 break;
             }
         }
 
-        if(tag_exists_globally) {
+        if(tag_exists_already) {
             fprintf(stderr, "Error: file already assigned this tag.\n");
             return -1;
         } else {
@@ -219,17 +214,17 @@ int add_tag(const char *filename, const char *tag) {
         json_object_array_add(all_tags_array, json_object_new_string(tag));
         json_object_object_add(db, all_tags, all_tags_array);
     } else {
-        int tag_exists_already = 0;     
+        int tag_exists_globally = 0;     
             for (int i = 0; i < json_object_array_length(all_tags_entry); i++ ) {
                 json_object* current_tag = json_object_array_get_idx(all_tags_entry, i);
 
                 if (strcmp(json_object_get_string(current_tag), tag) == 0) {
-                    tag_exists_already = 1;
+                    tag_exists_globally = 1;
                     break;
                 }
             }
 
-        if(!tag_exists_already) {
+        if(!tag_exists_globally) {
             json_object_array_add(all_tags_entry, json_object_new_string(tag));
             fprintf(stdout, "New tag detected: added to the list of all tags.\n");
         }
@@ -241,6 +236,7 @@ int add_tag(const char *filename, const char *tag) {
     return 0;
 }
 
+// TO-DO: sync globally
 int remove_tag(const char *filename, const char *tag) {
     if (!check_file_exists(filename)) {
         fprintf(stderr, "Error: file does not exist.\n");
@@ -252,6 +248,40 @@ int remove_tag(const char *filename, const char *tag) {
         fprintf(stderr, "Error: could not load the database.\n");
         return -1;
     }
+
+    json_object* file_entry;
+    json_object_object_get_ex(db, filename, &file_entry);
+
+    if (!file_entry) {
+        fprintf(stderr, "Error: file has no assigned tags.\n");
+        return -1;
+    } else {
+        int tag_exists_already = 0;     
+        for (int i = 0; i < json_object_array_length(file_entry); i++ ) {
+            json_object* current_tag = json_object_array_get_idx(file_entry, i);
+
+            if (strcmp(json_object_get_string(current_tag), tag) == 0) {
+                tag_exists_already = 1;
+                break;
+            }
+        }
+
+        if(tag_exists_already) {
+            fprintf(stderr, "Error: file already assigned this tag.\n");
+            return -1;
+        } else {
+            json_object_array_add(file_entry, json_object_new_string(tag));
+        }
+    }
+
+    // TO-DO: sync globally
+
+
+    fprintf(stdout, "Success: removed tag from file\n");
+    save_db(db);
+    json_object_put(db);
+    return 0;
+
 }
 
 int search_by_tag(const char *tag) {
@@ -262,12 +292,50 @@ int search_by_tag(const char *tag) {
     }
 }
 
-int list_all_tags() {
+/*
+    Returns a dynamically allocated array of strings.
+    The number of tags is stored in "*count_out".
+    Returns 0 on success, -1 on failure.
+*/
+int list_all_tags(char*** list_all_tags, size_t* count_out) {
+    const char* all_tags = "/__all_tags__";
+
     json_object* db = load_tag_db();
     if (!db) {
         fprintf(stderr, "Error: could not load the database.\n");
         return -1;
     }
+
+    json_object* all_tags_entry;
+    json_object_object_get_ex(db, all_tags, &all_tags_entry);
+
+    size_t count_unique_tags = 0;
+
+    if (!all_tags_entry) {
+        // "/__all_tags__" does not exist yet.
+        *count_out = 0;
+        json_object_put(db);
+        fprintf(stderr, "Error: there is no entry for all tags in the DB.\n");
+        return -1;
+    } else {    
+        count_unique_tags = json_object_array_length(all_tags_entry); 
+        *count_out = count_unique_tags;
+        *list_all_tags = malloc(count_unique_tags * sizeof(char*));
+
+        if (!(*list_all_tags)) {
+            json_object_put(db);
+            fprintf(stderr, "Error: could not allocate memory for the list of tags.\n");
+            return -1;
+        }
+
+        for (int i = 0; i < count_unique_tags; i++) {
+            json_object* current_entry = json_object_array_get_idx(all_tags_entry, i);
+            (*list_all_tags)[i] = strdup(json_object_get_string(current_entry)); 
+        }
+    }
+
+    json_object_put(db);
+    return 0;
 }
 
 int list_file_tags(const char *filename) {
